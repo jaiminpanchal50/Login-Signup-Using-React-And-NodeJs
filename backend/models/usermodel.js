37bf24ea4ba18c76c1be7859e0423bfef6054826
Jaimin Panchal <jaiminp.thoughtmate@gmail.com>
2024-10-23 16:14:22 +0530

    DB Url changes as pr requirement

--- backend/models/usermodel.js
+++ backend/models/usermodel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/test')
+mongoose.connect('mongodb://127.0.0.1:27017/test') //// change DB as par your requirement
 
 // Define the user schema
 const userSchema = new mongoose.Schema({
